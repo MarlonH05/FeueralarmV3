23ba1d15ed19cabd44ecbba6f10a436bf94db3d2
Marlon Landsiedel <marlon.landsiedel@p36.io>
2025-11-01 19:26:39 +0100

    Setup default inonic blanco app

---
+++ Frontend/src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./pages/settings/settings.page').then( m => m.SettingsPage)
+  },
+  {
+    path: 'information',
+    loadComponent: () => import('./pages/information/information.page').then( m => m.InformationPage)
+  },
+];
