7f4760e1679255ffd5956e292670c5159f9f2260
Marlon Landsiedel <marlon.landsiedel@p36.io>
2025-11-04 21:43:24 +0100

    App route redirect to login

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { authGuard, loginGuard } from './guards/auth.guard';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
   },
   {
